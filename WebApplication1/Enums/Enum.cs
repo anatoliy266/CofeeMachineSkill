2a7b48b8be7d06028dfd3399f4bf3cc95400e031
anatoliy266 <stakan993@gmail.com>
2019-08-20 17:57:05 +0300

    skillcat

---
+++ WebApplication1/Enums/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliceCofeemaker.Enums
+{
+    public enum SkillCategory
+    {
+        cofeeMachine = 0,
+        secretary,
+        officiant,
+        lot,
+        start,
+        unknown
+    }
+}
--- WebApplication1/Services/AnswerGenerator.cs
+++ WebApplication1/Services/AnswerGenerator.cs
@@ -5,17 +5,22 @@ using System.Net;
 using System.Threading.Tasks;
 using AliseCofeemaker.Models;
 using AliseCofeemaker.Modules;
+using AliceCofeemaker.Enums;
 
 namespace AliseCofeemaker.Services
 {
     public interface IAnswerGenerator
     {
-        string Generate(string[] tokens, EntityModel[] entities = null);
+        string Generate();
+
+        void SetTokens(string[] tokens);
     }
 
     public class AnswerGenerator : IAnswerGenerator
     {
         private IDBProcCaller procCaller;
+        private SkillCategory category;
+        private string[] words;
         public AnswerGenerator(IDBProcCaller caller)
         {
             procCaller = caller;
@@ -29,45 +34,82 @@ namespace AliseCofeemaker.Services
         /// <param name="tokens">массив слов запроса</param>
         /// <param name="entities">массив определенных процессором алисы сущностей</param>
         /// <returns>подготовленный ответ</returns>
-        public string Generate(string[] tokens = null, EntityModel[] entities = null)
+        public string Generate()
         {
             string reply = "";
+
+            switch (category)
+            {
+                case SkillCategory.start: //пустое сообщение на входе
+                    {
+                        reply = "Привет, я голосовой офисный помошник. Можете попросить меня сварить кофе, заказать обед или записать на прием";
+                        break;
+                    }
+                case SkillCategory.cofeeMachine: //свари/сделай/приготовь кофе
+                    {
+                        reply = CofeeMaker();
+                        break;
+                    }
+                case SkillCategory.unknown: //неизвестный запрос
+                    {
+                        reply = "Не понимаю о чем вы, попробуйте еще раз";
+                        break;
+                    }
+            }
+            
+            return reply;
+        }
+
+        public void SetTokens(string[] tokens)
+        {
+            if (//category не пустая)
+            {
+
+            }
+
             if (tokens.Count() == 0)
             {
-                //на входе пустое сообщение(активация навыка)
-                //в ответ приветственное сообщение
-                reply = "Привет, я голосовой офисный помошник. Можете попросить меня сварить кофе, заказать обед или записать на прием";
-            } else
+                category = SkillCategory.start;
+            }
+            else
             {
                 //на входе не пустое сообщение
                 if (tokens.Contains("кофе") && tokens.Intersect(new string[] { "свари", "сделай", "приготовь" }).Count() > 0)
                 {
-                    //запрос к базе, возращает DataSet с олонкой status
-                    var result = procCaller.Call("CofeeMashineStatus");
-
-                    if (result.Tables[0].Rows[0].ItemArray[result.Tables[0].Columns.IndexOf("status")].ToString() == "ok")
-                    {
-                        //status == ok
-
-                        //GET запрос на внешний ресурс
-                        WebClient client = new WebClient();
-                        client.UseDefaultCredentials = true;
-                        client.Headers.Add(HttpRequestHeader.Allow, "GET");
-                        client.DownloadData("https://someurl.net");
-
-                        //возвращаемый ответ
-                        reply = "Задача передана кофе-машине";
-                    }
-                    else
-                    {
-                        //status != ok, кофемашина занята
-
-                        //ответ = значение из поля status
-                        reply = result.Tables[0].Rows[0].ItemArray[result.Tables[0].Columns.IndexOf("status")].ToString();
-                    }
-                } 
+                    category = SkillCategory.cofeeMachine;
+                }
+                else
+                {
+                    category = SkillCategory.unknown;
+                }
             }
-            return reply;
+            words = tokens;
+        }
+
+        private string CofeeMaker()
+        {
+            //запрос к базе, возращает DataSet с олонкой status
+            var result = procCaller.Call("CofeeMashineStatus");
+
+            if (result.Tables[0].Rows[0].ItemArray[result.Tables[0].Columns.IndexOf("status")].ToString() == "ok")
+            {
+                //status == ok
+                //GET запрос на внешний ресурс
+                WebClient client = new WebClient();
+                client.UseDefaultCredentials = true;
+                client.Headers.Add(HttpRequestHeader.Allow, "GET");
+                client.DownloadData("https://someurl.net");
+
+                //возвращаемый ответ
+                return "Задача передана кофе-машине";
+            }
+            else
+            {
+                //status != ok, кофемашина занята
+
+                //ответ = значение из поля status
+                return result.Tables[0].Rows[0].ItemArray[result.Tables[0].Columns.IndexOf("status")].ToString();
+            }
         }
     }
 }
